0a61190cc998fca6a719830adfff1f414ffd740f
Iuri <iurischmoeller@hotmail.com>
2023-05-19 11:56:38 -0300

    Adicionados novos arquivos de controller e handler de web_sockets

---
+++ Controllers/WebSocketController.cs
@@ -0,0 +1,57 @@
+﻿using APISimples.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace APISimples.Controllers
+{
+    public class WebSocketController : ControllerBase
+    {
+
+        private readonly WSocketHandler _webSocketHandler;
+
+        public WebSocketController(WSocketHandler ws) 
+        {
+            _webSocketHandler = ws;
+        }
+
+        [Route("/ws")]
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest){
+                var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+
+                int userId = int.Parse(HttpContext.Request.Query["userId"]);
+                _webSocketHandler.AdicionarSocket(userId, webSocket);
+                await Echo(webSocket, userId);
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        private async Task Echo(WebSocket webSocket, int id)
+        {
+            var buffer = new byte[1024];
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!result.CloseStatus.HasValue)
+            {
+               var data = Encoding.ASCII.GetBytes("ping");
+
+                await webSocket.SendAsync(data, WebSocketMessageType.Text, true, CancellationToken.None);
+
+                await Task.Delay(30000);
+
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            _webSocketHandler.RemoverSocket(id);
+
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+        }
+    }
+}
